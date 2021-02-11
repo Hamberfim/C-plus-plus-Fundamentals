2a28615786c21e1f3c0cf7e2c8a25dacf5e20e5b
Hamberfim <adhamlingithub@gmail.com>
2021-02-11 13:04:26 -0600

    updated comments

--- variableArrays/main.cpp
+++ variableArrays/main.cpp
@@ -3,18 +3,19 @@ using namespace std;
 
 int main()
 {
-    // using variable arrays
+    // using variable arrays - size canot be changed
     // declared and initialized
     int numbers[4] = {0, 1, 2, 3};
 
     //declared and initialized
     float flts[3];
+    // add items to the array
     flts[0] = 1.2;
     flts[1] = 1.7;
     flts[2] = 2.1;
 
     // character array - used less than the string class
-    char myName[8] = {'A', 'n', 't', 'h', 'o', 'n', 'y', '\0'};
+    char myName[8] = {'A', 'n', 't', 'h', 'o', 'n', 'y', '\0'};  // '\0' <-special null char at end stores the array as a string
 
     // two-dimentional array - 2col, 3rows
     int coordinates[2][3] = {{1, 2, 3}, {4, 5, 6}};
